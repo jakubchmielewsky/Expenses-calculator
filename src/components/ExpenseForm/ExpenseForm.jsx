1fdc0042c542ad7cec4983cadf7e58660b6e9f00
Jakub Chmielwski <kuba91821@gmail.com>
2024-09-19 15:52:24 +0200

    add state and handlers in expense form

--- src/components/ExpenseForm/ExpenseForm.jsx
+++ src/components/ExpenseForm/ExpenseForm.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+
+//components
 import { ExpenseFormContainer } from "./ExpenseForm.style";
 import Button from "../Shared/Button";
 import InputGroup from "../Shared/InputGroup"
 
+//context
+import { ExpenseContext } from "../../context/ExpenseContext";
+
+
+
 export default function ExpenseForm(params) {
+    const {addExpense} = useContext(ExpenseContext);
+
+    const [date,setDate]= useState("");
+    const [expense,setExpense]= useState("");
+    const [amount,setAmount]= useState("");
+
+    const handleDateChange = (e)=>{
+        setDate(e.target.value);
+    };
+
+    const handleExpenseChange = (e)=>{
+        setExpense(e.target.value);
+    };
+
+    const handleAmountChange = (e)=>{
+        setAmount(e.target.value);
+        console.log(amount);
+    };
+
+    const handleAddClick= ()=>{
+
+    }
+
     return (
         <ExpenseFormContainer>
-            <InputGroup label={"Date"} />
-            <InputGroup label={"Expense"}/>
-            <InputGroup label={"Amount"}/>
-            <Button style={{backgroundColor: 'green'}} text="Add"/>
+            <InputGroup label={"Date"} onChange={handleDateChange} value={date} type={"date"}/>
+            <InputGroup label={"Expense"} onChange={handleExpenseChange} value={expense} type={"text"}/>
+            <InputGroup label={"Amount"} onChange={handleAmountChange} value={amount} type={"number"}/>
+            <Button style={{backgroundColor: 'green'}} text="Add" onClick={handleAddClick}/>
         </ExpenseFormContainer>
     )
 }
